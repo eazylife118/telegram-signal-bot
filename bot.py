d8e70b7bf6498bac4b8d43c1f1ab79ad191b89c2
eazylife118 <babyloneazylife@gmail.com>
2026-06-23 12:44:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,49 @@
 import requests
 import time
+import random
 BOT_TOKEN = "8612354100:AAFUTlaSiq19yycQWpO70J4d6DEbgF4Kicc"
 CHAT_ID = "6280535707"
 
+pairs = ["EURUSD", "GBPUSD", "USDJPY", "AUDUSD", "USDCAD"]
+
+directions = ["BUY", "SELL"]
+
+times = ["15s", "30s", "1m", "2m", "3m", "5m", "15m"]
+
 while True:
+
+    pair = random.choice(pairs)
+
+    direction = random.choice(directions)
+
+    tf = random.choice(times)
+
+    message = f"""📊 SIGNAL ALERT
+
+Pair: {pair}
+
+Direction: {direction}
+
+Timeframe: {tf}
+
+⚠️ Test Signal
+
+"""
+
     requests.get(
+
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+
         params={
+
             "chat_id": CHAT_ID,
-            "text": "✅ Test message from Render"
+
+            "text": message
+
         }
+
     )
 
-    print("Message sent")
-    time.sleep(60)
+    print("Signal sent")
+
+    time.sleep(120)
