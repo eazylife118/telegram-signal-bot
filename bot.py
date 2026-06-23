f7b5eb7f09cfc97458a507236d44f615019449c5
eazylife118 <babyloneazylife@gmail.com>
2026-06-23 18:13:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
 import requests
-import os
 
-BOT_TOKEN = os.getenv("8612354100:AAFUTlaSiq19yycQWpO70J4d6DEbgF4Kicc")
-CHAT_ID = os.getenv("6280535707")
+BOT_TOKEN = "8612354100:AAFUTlaSiq19yycQWpO70J4d6DEbgF4Kicc"
+CHAT_ID = "6280535707"
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": text
-        }
-    )
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Bot is working!"
+    }
+)
 
-send_message("✅ Bot is working!")
+print(response.text)
