2988364dcb6f05a314a73d721358f0bd00572b0c
eazylife118 <babyloneazylife@gmail.com>
2026-06-23 17:07:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ BOT_TOKEN = os.getenv("8612354100:AAFUTlaSiq19yycQWpO70J4d6DEbgF4Kicc")
 CHAT_ID = "6280535707"
 
 def send_message(text):
-url = f”https://api.telegram.org/bot{BOT_TOKEN}/sendMessage”
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 requests.post(url, data={
 “chat_id”: CHAT_ID,
 “text”: text
