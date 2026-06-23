a44e3623b04d2447483d85317673035376fbfaf7
eazylife118 <babyloneazylife@gmail.com>
2026-06-23 10:57:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Telegram Signal Bot Started")
+import requests
+import time
+BOT_TOKEN = "8612354100:AAEuBmuJGHvf9Lw05cC7VafVs0OMXtDIAGs"
+CHAT_ID = "6280535707"
+
+while True:
+    requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={
+            "chat_id": CHAT_ID,
+            "text": "✅ Test message from Render"
+        }
+    )
+
+    print("Message sent")
+    time.sleep(60)
