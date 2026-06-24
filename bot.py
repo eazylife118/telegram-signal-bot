dcb438de3cba3129b4fd748f66c71079264bf7b7
eazylife118 <babyloneazylife@gmail.com>
2026-06-24 10:05:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,8 +32,8 @@ timeframes = ["1m", "2m", "3m", "4m", "5m"]
 
 def send_signal():
 pair = random.choice(pairs)
-direction = random.choice([“BUY”, “SELL”])
-expiry = random.choice([“1”, “2”, “3”, “5”])
+direction = random.choice(["BUY", "SELL"])
+expiry = random.choice(["1", "2", "3", "5"])
 strength = random.randint(75, 95)
 
 current_time = time.time()
@@ -44,7 +44,7 @@ message = f"""
 Pair: {pair}
 Direction: {direction}
 
-Entry Time: {time.strftime(’%H:%M’)}
+Entry Time: {time.strftime('%H:%M')}
 
 Trade Time: {time.strftime(’%H:%M’, time.localtime(current_time + 60))}
 
