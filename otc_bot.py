d8b8c1089efe3a371905f8f6ad91399e19c719d9
eazylife118 <babyloneazylife@gmail.com>
2026-07-06 00:49:26 +0100

    Update otc_bot.py

--- otc_bot.py
+++ otc_bot.py
@@ -4,6 +4,7 @@ import requests
 # YOUR CREDENTIALS
 # ==========================================
 TOKEN = "8846196749:AAHbfxiFqW49b0Ap2yZ2Vt_qE1wlfQ4os6s"
+CHAT_ID = "6280535707"
 CHANNEL_ID = "-1004324805205"  # Your channel ID
 
 def send_test_message():
